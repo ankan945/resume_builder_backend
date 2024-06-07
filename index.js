c9f935e078020e7fd2a86201782281d3f3735e99
ankan945 <119874965+ankan945@users.noreply.github.com>
2024-06-07 17:17:43 +0530

    changes

--- index.js
+++ index.js
@@ -1,51 +1,37 @@
 const express = require('express');
 const cors = require("cors");
-const pdf = require('html-pdf');
+const pdf = require ('html-pdf');
 const pdfsample = require('./pdf-sample');
 
+
 const app = express();
+
+
 const port = process.env.PORT || 4005;
 
-const allowedOrigins = ['https://heroic-basbousa-e35e13.netlify.app/'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 
-app.post("/create-pdf", (req, res) => {
-  console.log('Received request to create PDF with data:', req.body);
 
-  pdf.create(pdfsample(req.body), {}).toFile("Resume.pdf", (err, result) => {
-    if (err) {
-      console.error('Error creating PDF:', err);
-      return res.status(500).send(Promise.reject());
-    }
-    console.log('PDF created successfully:', result);
-    res.send(Promise.resolve());
-  });
+app.post("/create-pdf",(req,res)=> {
+    pdf.create(pdfsample(req.body),{}).toFile("Resume.pdf",(err)=> {
+        if(err){
+            res.send(Promise.reject());
+            console.log(err);
+        }
+        res.send(Promise.resolve());
+        console.log("Success");
+    });
 });
 
 app.get("/fetch-pdf", (req, res) => {
-  const filePath = `${__dirname}/Resume.pdf`;
-  console.log('Fetching PDF from:', filePath);
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error('Error sending PDF file:', err);
-      res.status(500).send(err);
-    }
+    res.sendFile(`${__dirname}/Resume.pdf`);
   });
-});
 
-app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
+
+//app.use(express.static("../frontend/build"));
+
+app.listen(port ,()=>{
+    console.log(`Server is running at port ${port}`);
 });
